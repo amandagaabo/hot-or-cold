27fe0ac6fd2e1ad7d1fc8224053518984d42708c
Amanda Herschleb <amanda.herschleb@gmail.com>
2017-11-24 12:42:58 -0700

    added guess-form tests

---
+++ src/components/guess-form.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import GuessForm from './guess-form';
+
+describe('<GuessForm />', () => {
+    it('Renders without crashing', () => {
+        shallow(<GuessForm />);
+    });
+
+    it('Should call onChange when input text changes', () => {
+        const callback = jest.fn();
+        const wrapper = shallow(<GuessForm onChange={callback} />);
+        const inputField = wrapper.find('input[type="number"]');
+        inputField.simulate('change', {target: {value:'1'} });
+        expect(callback).toHaveBeenCalledTimes(1);
+        inputField.simulate('change', {target: {value: '1'}});
+        expect(callback).toHaveBeenCalledTimes(2);
+    });
+
+    it('Should call onSubmit when form is submitted', () => {
+        const callback = jest.fn();
+        const callback2 = jest.fn();
+        const wrapper = mount(<GuessForm onSubmit={callback} onChange={callback2} />);
+        const inputField = wrapper.find('input[type="number"]');
+        inputField.simulate('change', {target: {value: '1'}});
+        expect(callback2).toHaveBeenCalledTimes(1);
+
+        wrapper.find('form').simulate('submit');
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+});
