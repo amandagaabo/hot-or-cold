854ba448e93ef935144726aefb38f3d44f967985
Amanda Herschleb <amanda.herschleb@gmail.com>
2017-11-24 13:09:21 -0700

    added game tests, all tests added

---
+++ src/components/game.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Game from './game';
+
+describe('<Game />', () => {
+  it('Renders without crashing', () => {
+      shallow(<Game />);
+  });
+
+  it('Handles generateRandom()', () => {
+    const wrapper = shallow(<Game />);
+    wrapper.instance().generateRandom();
+    expect(wrapper.state('targetNumber')).toBeGreaterThan(0);
+    expect(wrapper.state('targetNumber')).toBeLessThan(101);
+  });
+
+  it('Handles updateCurrentGuess()', () => {
+    const wrapper = shallow(<Game />);
+    wrapper.setState({
+      'targetNumber': 50
+    })
+    wrapper.update()
+
+    wrapper.instance().updateCurrentGuess(51);
+    expect(wrapper.state('currentGuess')).toEqual({"number": 51, "result": "hot"});
+
+    wrapper.instance().updateCurrentGuess(75);
+    expect(wrapper.state('currentGuess')).toEqual({"number": 75, "result": "cold"});
+
+    wrapper.instance().updateCurrentGuess(50);
+    expect(wrapper.state('currentGuess')).toEqual({"number": 50, "result": "correct"});
+  });
+
+  it('Handles onSubmit()', () => {
+    const wrapper = shallow(<Game />);
+    wrapper.setState({
+      'targetNumber': 50,
+      'currentGuess': {
+        'number': 50,
+        'result': 'correct'
+      },
+      'allGuesses': [{"number": 51, "result": "hot"}, {"number": 75, "result": "cold"}]
+    })
+
+    const updatedGuesses = [
+      {"number": 51, "result": "hot"},
+      {"number": 75, "result": "cold"},
+      {"number": 50, "result": "correct"}
+    ]
+
+    wrapper.update()
+
+    wrapper.instance().onSubmit()
+    expect(wrapper.state('currentGuess')).toEqual({'number': "", 'result': ""})
+    expect(wrapper.state('allGuesses')).toEqual(updatedGuesses)
+  });
+
+  it('Handles resetGame()', () => {
+    const wrapper = shallow(<Game />);
+    wrapper.setState({
+      'targetNumber': -1,
+      'currentGuess': {
+        'number': -1,
+        'result': 'correct'
+      },
+      'allGuesses': [{"number": 51, "result": "hot"}, {"number": 75, "result": "cold"}]
+    })
+    wrapper.update()
+
+    wrapper.instance().resetGame()
+    expect(wrapper.state('targetNumber')).toBeGreaterThan(0);
+    expect(wrapper.state('targetNumber')).toBeLessThan(101);
+    expect(wrapper.state('currentGuess')).toEqual({'number': "", 'result': ""})
+    expect(wrapper.state('allGuesses')).toEqual([])
+  });
+});
