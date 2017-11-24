2474d56039fa0153fd1a8291a646380cefe55ded
Amanda Herschleb <amanda.herschleb@gmail.com>
2017-11-24 12:26:37 -0700

    added play-again tests

---
+++ src/components/play-again.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import PlayAgain from './play-again';
+
+describe('<PlayAgain />', () => {
+    it('Renders without crashing', () => {
+        shallow(<PlayAgain />);
+    });
+
+    it('Should call onClick when play again button is clicked', () => {
+        const callback = jest.fn();
+        const wrapper = shallow(<PlayAgain onClick={callback} />);
+        const button = wrapper.find('button');
+        button.simulate('click');
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+});
